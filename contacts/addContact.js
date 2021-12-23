f4de563997f2388a1cb0ff94fa52bad4608b10ca
Oleksandr Leiberiuk <Lejberyk.ua@gmail.com>
2021-12-23 21:57:41 +0200

    change addContacts

--- contacts/addContact.js
+++ contacts/addContact.js
@@ -1,10 +1,11 @@
+const {nanoid} = require("nanoid");
 const listContacts = require("./listContacts");
 const updateContacts = require("./updateContacts");
 
 async function addContact({name, email, phone}) {
     const contacts = await listContacts();
     const newContact ={
-        id: String(contacts.length +1),
+        id: nanoid(),
         name,
         email,
         phone,
